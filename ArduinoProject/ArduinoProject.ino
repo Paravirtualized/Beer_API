d2b62f0772466e1863f3f4273cc798ab6bc6a543
Paravirtualized <113600934+Paravirtualized@users.noreply.github.com>
2020-05-26 14:16:00 +0000

    Minor text fixes.

--- ArduinoProject/ArduinoProject.ino
+++ ArduinoProject/ArduinoProject.ino
@@ -20,4 +20,5 @@ void setup() {
 
   // This is only used if you want to attempt to let the Arduino generate the 4Mhz clock
   // that the SN76489 requires to operate.  NOTE: Beacuse this uses a custom PWM to generate it
-  // It MUST use pin 10 as in the clock define above.  
+  // It MUST use pin 10 as in the clock define above.  If you have a hardware 4mhz clock
+  // device on the board however, you can leave the 
