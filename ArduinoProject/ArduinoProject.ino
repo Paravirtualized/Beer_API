13f8553aaf57b6fba13e6093b14f512c8886f225
Paravirtualized <113600934+Paravirtualized@users.noreply.github.com>
2020-10-10 05:34:56 +0000

    Update MetrolookTemplate.php

--- ArduinoProject/ArduinoProject.ino
+++ ArduinoProject/ArduinoProject.ino
@@ -98,4 +98,5 @@ void InitiliseSoundChip()
   delay(20);
 
   // Next block of commands to the sound chip
-  SendByteToSoundChip(0x92); // Set volume on tone 
+  SendByteToSoundChip(0x92); // Set volume on tone channel 3 to 2
+  SendByteToSoundChip(0x8F); // First byte of set Frequency on Tone 3 to
