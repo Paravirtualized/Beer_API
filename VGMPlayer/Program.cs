dbcdcdbbb66e9f8a8aeaf22c4a1e0f5a15d5f033
Paravirtualized <113600934+Paravirtualized@users.noreply.github.com>
2022-09-09 07:10:14 +0000

    Update permissions.md

--- VGMPlayer/Program.cs
+++ VGMPlayer/Program.cs
@@ -3,4 +3,10 @@ using System.Threading;
 
 // VGM Player written by
 // !Shawty!/DS in 2017
-// NOTE: This player is designed only to playback BBC M
+// NOTE: This player is designed only to playback BBC Model B VGM Files
+
+namespace VGMPlayer
+{
+  class Program
+  {
+    
